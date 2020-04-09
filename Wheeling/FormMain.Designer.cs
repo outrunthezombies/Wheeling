1a99c90e8d5c7505e65ad3d16727ca8d7ae9f7b5
Greg Salsman <gbsalsman@gmail.com>
2020-04-09 12:26:14 -0400

    Show all historical data
    https://outrunthezombies.atlassian.net/browse/WHEEL-1

--- Wheeling/FormMain.Designer.cs
+++ Wheeling/FormMain.Designer.cs
@@ -28,20 +28,192 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loadLotteryResultsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.DgvLotteryData = new System.Windows.Forms.DataGridView();
+            this.LblLotteryName = new System.Windows.Forms.Label();
+            this.Date = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.N1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.N2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.N3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.N4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.N5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.N6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.N7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Bonus = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.DgvLotteryData)).BeginInit();
             this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1065, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "MnuMainMenu";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.loadLotteryResultsToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // loadLotteryResultsToolStripMenuItem
+            // 
+            this.loadLotteryResultsToolStripMenuItem.Name = "loadLotteryResultsToolStripMenuItem";
+            this.loadLotteryResultsToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.loadLotteryResultsToolStripMenuItem.Text = "Load Lottery Results";
+            this.loadLotteryResultsToolStripMenuItem.Click += new System.EventHandler(this.LoadLotteryResultsToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.ExitToolStripMenuItem_Click);
+            // 
+            // DgvLotteryData
+            // 
+            this.DgvLotteryData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DgvLotteryData.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Date,
+            this.N1,
+            this.N2,
+            this.N3,
+            this.N4,
+            this.N5,
+            this.N6,
+            this.N7,
+            this.Bonus});
+            this.DgvLotteryData.Location = new System.Drawing.Point(12, 140);
+            this.DgvLotteryData.Name = "DgvLotteryData";
+            this.DgvLotteryData.Size = new System.Drawing.Size(547, 384);
+            this.DgvLotteryData.TabIndex = 1;
+            // 
+            // LblLotteryName
+            // 
+            this.LblLotteryName.AutoSize = true;
+            this.LblLotteryName.Location = new System.Drawing.Point(12, 121);
+            this.LblLotteryName.Name = "LblLotteryName";
+            this.LblLotteryName.Size = new System.Drawing.Size(0, 13);
+            this.LblLotteryName.TabIndex = 2;
+            // 
+            // Date
+            // 
+            this.Date.HeaderText = "Date";
+            this.Date.Name = "Date";
+            this.Date.ReadOnly = true;
+            this.Date.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.Date.Width = 80;
+            // 
+            // N1
+            // 
+            this.N1.HeaderText = "N1";
+            this.N1.Name = "N1";
+            this.N1.ReadOnly = true;
+            this.N1.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.N1.Width = 50;
+            // 
+            // N2
+            // 
+            this.N2.HeaderText = "N2";
+            this.N2.Name = "N2";
+            this.N2.ReadOnly = true;
+            this.N2.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.N2.Width = 50;
+            // 
+            // N3
+            // 
+            this.N3.HeaderText = "N3";
+            this.N3.Name = "N3";
+            this.N3.ReadOnly = true;
+            this.N3.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.N3.Width = 50;
+            // 
+            // N4
+            // 
+            this.N4.HeaderText = "N4";
+            this.N4.Name = "N4";
+            this.N4.ReadOnly = true;
+            this.N4.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.N4.Width = 50;
+            // 
+            // N5
+            // 
+            this.N5.HeaderText = "N5";
+            this.N5.Name = "N5";
+            this.N5.ReadOnly = true;
+            this.N5.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.N5.Width = 50;
+            // 
+            // N6
+            // 
+            this.N6.HeaderText = "N6";
+            this.N6.Name = "N6";
+            this.N6.ReadOnly = true;
+            this.N6.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.N6.Width = 50;
+            // 
+            // N7
+            // 
+            this.N7.HeaderText = "N7";
+            this.N7.Name = "N7";
+            this.N7.ReadOnly = true;
+            this.N7.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.N7.Width = 50;
+            // 
+            // Bonus
+            // 
+            this.Bonus.HeaderText = "Bonus";
+            this.Bonus.Name = "Bonus";
+            this.Bonus.ReadOnly = true;
+            this.Bonus.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.Bonus.Width = 50;
             // 
             // FormMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1065, 580);
+            this.Controls.Add(this.LblLotteryName);
+            this.Controls.Add(this.DgvLotteryData);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "FormMain";
             this.Text = "Lottery Wheeling";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.DgvLotteryData)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.DataGridView DgvLotteryData;
+        private System.Windows.Forms.ToolStripMenuItem loadLotteryResultsToolStripMenuItem;
+        private System.Windows.Forms.Label LblLotteryName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Date;
+        private System.Windows.Forms.DataGridViewTextBoxColumn N1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn N2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn N3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn N4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn N5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn N6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn N7;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Bonus;
     }
 }
 
--- Wheeling/FormMain.cs
+++ Wheeling/FormMain.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.OleDb;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,124 @@ namespace Wheeling
 {
     public partial class FormMain : Form
     {
+        private OleDbConnection oOleDbConnection;
+        private OleDbCommand oOleDbCommand;
+        private readonly string sConnectionString = "Provider=Microsoft.ACE.OLEDB.12.0; Data Source=C:\\Users\\Greg\\source\\repos\\Wheeling\\Wheeling\\wheeling.accdb;Persist Security Info=False;";
+        private string lotteryName;
+        private enum DataColumn
+        {
+            LotteryName = 0,
+            DrawDate = 1,
+            N1 = 2,
+            N2 = 3,
+            N3 = 4,
+            N4 = 5,
+            N5 = 6,
+            N6 = 7,
+            N7 = 8,
+            Bonus = 9
+        }
         public FormMain()
         {
             InitializeComponent();
+        }
+        private void ExitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void LoadLotteryResultsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            LoadLotteryData(1);
+        }
+        private Boolean OpenDBConnection(string sql)
+        {
+            try
+            {
+                oOleDbConnection = new OleDbConnection();
+                oOleDbConnection.ConnectionString = sConnectionString;
+                oOleDbConnection.Open();
+                if (oOleDbConnection.State == ConnectionState.Open)
+                {
+                    oOleDbCommand = new OleDbCommand(sql, oOleDbConnection);
+                }
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                oOleDbCommand = null;
+                oOleDbConnection = null;
+                Console.WriteLine(ex.StackTrace);
+
+                return false;
+            }
+        }
+
+        private void CloseDBConnection()
+        {
+            try
+            {
+                if (oOleDbConnection != null && oOleDbConnection.State == ConnectionState.Open)
+                    oOleDbConnection.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.StackTrace);
+            }
+            finally
+            {
+                oOleDbCommand = null;
+                oOleDbConnection = null;
+            }
+        }
+        private void LoadLotteryData(int lottery)
+        {
+            string sql = "SELECT L.lottery_name,D.Date,D.N1,D.N2,D.N3,D.N4,D.N5,D.N6,D.N7,D.Bonus FROM Lottery L INNER JOIN Draws D ON L.ID = D.Lottery_ID WHERE L.ID = " + lottery + " ORDER BY D.Date DESC";
+            this.DgvLotteryData.Rows.Clear();
+
+            OleDbDataReader oOleDbDataReader;
+            try
+            {
+                if (OpenDBConnection(sql))
+                {
+                    oOleDbDataReader = oOleDbCommand.ExecuteReader();
+
+                    while (oOleDbDataReader.Read())
+                    {
+                        int field = oOleDbDataReader.FieldCount;
+                        string[] rowData = new string[field];
+                        int column = 0;
+                        lotteryName = oOleDbDataReader.GetString((int)DataColumn.LotteryName);
+                        for (int index=0; index<field; index++)
+                        {
+                            if (index != (int)DataColumn.LotteryName)
+                            {
+                                if (index == (int)DataColumn.DrawDate)
+                                {
+                                    rowData[column] = oOleDbDataReader.GetDateTime(index).ToString("MMM. dd, yyyy");
+                                } 
+                                else
+                                {
+                                    rowData[column] = oOleDbDataReader.GetValue(index).ToString();
+                                }
+                                column++;
+                            }
+                        }
+                        this.DgvLotteryData.Rows.Add(rowData);
+                    }
+                    LblLotteryName.Text = lotteryName;
+                    oOleDbDataReader.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.StackTrace);
+            }
+            finally
+            {
+                CloseDBConnection();
+            }
         }
     }
 }
